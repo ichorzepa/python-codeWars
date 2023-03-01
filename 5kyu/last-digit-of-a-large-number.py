fddd9e0f4e151427ffa0786e4f9487e2cd1c503f
ichorzepa <33585272+ichorzepa@users.noreply.github.com>
2023-03-01 08:45:50 -0500

    little edit

--- 5kyu/last-digit-of-a-large-number.py
+++ 5kyu/last-digit-of-a-large-number.py
@@ -4,15 +4,15 @@ def last_digit(n1, n2):
 	if(n2==0):
 		return 1
 	last = []
-    for i in range(1,10):
-    	if(str((n1**i))[-1] in last):
-    		break
-    	else:
-    		last.append(str((n1**i))[-1])
-    if(n2%len(last) != 0):
-    	return int(last[(n2 % len(last)) -1])
-    else:
-    	return int(last[len(last)-1])
+	for i in range(1,10):
+		if(str((n1**i))[-1] in last):
+			break
+		else:
+			last.append(str((n1**i))[-1])
+	if(n2%len(last) != 0):
+		return int(last[(n2 % len(last)) -1])
+	else:
+		return int(last[len(last)-1])
 
 
 print(last_digit(3715290469715693021198967285016729344580685479654510946723, 68819615221552997273737174557165657483427362207517952651))
